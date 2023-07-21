f554004d8bd08fdb7763769feb9079e7e610d5d7
Fatih <matematikcifth@gmail.com>
2023-07-21 14:17:51 +0200

    v2

---
+++ src/AtmProject.java
@@ -0,0 +1,21 @@
+public class AtmProject {
+    /*
+    ====> git restore --staged[dosya]       --->Stage Area'daki tek bir dosyayı iptal eder, bir onceki asamaya
+    (Working Space'e) gonderir.
+====> git restore --staged .            --->Stage Area'daki tüm dosyaları iptal eder
+====> git reset --hard                  ---->Working space deki değişiklikleri iptal eder, staging area yı boşaltır.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Bu bir Atm projesidir");
+        //version1
+
+        System.out.println("java kolaydir");
+
+        //version2g
+
+    }
+
+
+
+}
